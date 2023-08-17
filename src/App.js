6b48385407fa99a30e6c954e0574bcd5b977dbdd
Yash <chauhanyash4567@gmail.com>
2023-08-17 14:44:07 +0530

    completed almost full project

--- src/App.js
+++ src/App.js
@@ -1,41 +1,85 @@
-// import logo from './logo.svg';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import { Home } from "./Components/Home";
-import { User } from "./Components/User";
-import { Profile } from "./Components/Profile";
-import { ThemeProvider, createTheme } from "@material-ui/core";
-import { Register } from "./Components/Register/Register";
-
-function App() {
-  const name = "Yash";
-  const age = 19;
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#d32f2f",
-      },
-      secondary: {
-        main: "#d3f400",
-      },
-      
+import './App.css';
+import './index.js';
+import { Route, Routes, NavLink } from 'react-router-dom';
+import Register from './components/Register';
+import { CssBaseline, useTheme } from '@material-ui/core';
+import Home from './components/Home';
+import { purple } from '@material-ui/core/colors';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
+import Login from './components/Login';
+import Header from './components/Header';
+import { AuthProvider } from './contexts/auth';
+import { CartProvider } from './contexts/cartContext';
+import EditBook from './components/EditBook';
+import ProductPage from './components/ProductPage';
+import Users from './components/Users';
+import EditUser from './components/EditUser';
+import Category from './components/categoty';
+import EditCategory from './components/EditCategory';
+import Cart from './components/Cart';
+const theme = createTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      light: "#ff4081",
+      main: purple[600],
+      dark: "#c51162",
+      contrastText: "#fff"
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#11cb5f',
+    },
+  },
+  typography: {
+    fontFamily: 'Fira Sans',
+    fontSize: 15,
+    h1: {
+      fontWeight: 300,
+      fontSize: "6rem",
+      lineHeight: 1.167,
+      letterSpacing: "-0.01562em",
+      color: "#ff4081 !important",
     },
 
-  });
+  },
+});
+
+function App() {
+
+
+  // const theme=useTheme();
 
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}> 
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/user" element={<User name={name} age={age} />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+      <CartProvider>  
+      <Header/>
+      <ToastContainer />
+      <CssBaseline />
+      <Routes path='/'>
+        <Route index element={<Home />} />
+        {/* <Route path='/welcome' element={<Welcome />} /> */}
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/editBook/:id' element={<EditBook />} />
+        <Route path='/product' element={<ProductPage />} />
+        <Route path='/add-book' element={<EditBook />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/editUser/:id' element={<EditUser />} />
+        <Route path='/category' element={<Category />} />
+        <Route path='/editCategory/:id' element={<EditCategory />} />
+        <Route path='/add-category' element={<EditCategory />} />
+        <Route path='/cart' element={<Cart />} />
+
+      </Routes>
+
+      </CartProvider>
+      </AuthProvider>
+    </ThemeProvider>
+
   );
 }
 
